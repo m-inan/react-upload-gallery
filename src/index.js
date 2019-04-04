2c744b1910b3e1d096a2b6eb565834a6f4fbdcb6
minan <muhammet@tilpark.com>
2019-04-05 01:44:20 +0300

    Fixes name error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { default as DragArea } from './DragArea'
-export { default as DropArea } from './DragArea'
+export { default as DropArea } from './DropArea'
 
 export { default as Card } from './view/Card'
 export { default as List } from './view/List'
