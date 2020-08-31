fa95b0e16972b030063708a946f5d0a7db422200
minan <muhammet@tilpark.com>
2020-08-31 21:54:36 +0300

    use prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,33 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        index: './src/index.js',
-        style: './src/style.scss'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.*scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    externals: {
-        'react': 'commonjs react' 
-    }
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+    style: "./src/style.scss",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.*scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  externals: {
+    react: "commonjs react",
+  },
 };
+
